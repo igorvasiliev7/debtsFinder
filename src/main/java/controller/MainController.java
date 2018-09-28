09608a540d3914b19c9d10f393b64e12ecfcddb4
igor <igorvasiliev7"gmail.com>
2018-09-28 09:17:15 +0300

    11

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -10,6 +10,7 @@ import javafx.stage.FileChooser;
 import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -29,8 +30,8 @@ public class MainController implements Initializable{
     @FXML
     private Button btnSearch;
 
-    File debtorsFile;
-    File paymentsFile;
+    private File debtorsFile;
+    private File paymentsFile;
 
     public void initialize(URL location, ResourceBundle resources) {
         btnSearch.setOnAction(e-> search());
@@ -38,89 +39,94 @@ public class MainController implements Initializable{
         btnPathPayment.setOnAction(event -> choosePayments());
     }
 
+    private void search() {
+        String payment;
+        String debtor;
+        String paymentsShort = "";
+        String debtorShort = "";
+        List<String> list = new LinkedList<>();
+        String [] debtsArr;
+        String [] paymentsArr;
+
+        if(paymentsFile == null||debtorsFile == null){
+            txtError.setText("One of lists is not chosen");
+            return;
+        } else txtError.setText("Searching...");
+
+        BufferedReader readDebts;
+        BufferedReader readPayments = reader(paymentsFile);
+
+        try {
+            BufferedWriter result = writer("d://result.txt");
+
+            while ((payment = readPayments.readLine()) != null) {
+                readDebts = reader(debtorsFile);
+                list.clear();
+                payment = payment.toLowerCase();
+                while ((debtor = readDebts.readLine()) != null) {
+                    debtor = debtor.toLowerCase();
+                    paymentsArr = payment.split(";");
+                    debtsArr = debtor.split(";");
+
+                    if(paymentsArr.length>2&&debtsArr.length>2){
+                        paymentsShort=paymentsArr[2];
+                        debtorShort=debtsArr[2];
+                    }else continue;
+                    if (debtorShort.contains(paymentsShort)) {
+                        list.add(debtor);
+                    }
+                }
+                readDebts.close();
+                if (list.size() != 0) {
+                    txtResults.setText("Client: \n" + payment + "\nDebtors: ");
+                    result.write("Client: " + payment + "\nDebtors: \n");
+                    for (int i = 0; i < list.size(); i++) {
+                        txtResults.setText(list.get(i) + "\n\n");
+                        result.write(list.get(i) + "\n\n");
+                    }
+                }
+            }
+
+            readPayments.close();
+            result.close();
+            System.out.println("The End");
+        }
+        catch (IOException e){
+            System.out.println("Error in result.txt creating");
+        }
+    }
+
+    private BufferedWriter writer(String path) throws FileNotFoundException {
+        return new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(path)));
+    }
+    private BufferedReader reader(File file){
+        try {
+            return new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(file), "Cp1251"));
+             } catch (FileNotFoundException | UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+            return null;
+        }
+    }
+
     private void choosePayments() {
         FileChooser fc=new FileChooser();
         fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Csv files", "*.csv"));
-        debtorsFile = fc.showOpenDialog(null);
-        txtPathPayments.setText(debtorsFile.getPath());
+        paymentsFile = fc.showOpenDialog(null);
+       if(paymentsFile!=null) {
+           txtPathPayments.setText(paymentsFile.getPath());
+       }
     }
 
     private void chooseDebtors() {
         FileChooser fc=new FileChooser();
         fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Csv files", "*.csv"));
-        paymentsFile = fc.showOpenDialog(null);
-        txtPathDebtors.setText(paymentsFile.getPath());
-    }
-
-    private void search() {
-        String payment;
-        String debtor;
-        String paymentsShort;
-        String debtorShort;
-        List<String> list;
-
-        BufferedReader readPayments = null;
-        try {
-            readPayments = new BufferedReader(
-                    new InputStreamReader(
-                            new FileInputStream(paymentsFile), "Cp1251"));
-        } catch (UnsupportedEncodingException e) {
-            txtError.setText("Payment list is not chosen");
-            return;
-        } catch (FileNotFoundException e) {
-            txtError.setText("Payment list is not chosen");
-            return;
-        }
-
-        BufferedReader readDebts = null;
-        try {
-            readDebts = new BufferedReader(
-                    new InputStreamReader(
-                            new FileInputStream(debtorsFile), "Cp1251"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            txtError.setText("Debtors list is not chosen");
-            return;
-        }
-
-        BufferedWriter result = null;
-        try {
-            result = new BufferedWriter(
-                    new OutputStreamWriter(
-                            new FileOutputStream("/results.txt"), "Cp1251"));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-try {
-
-
-        while ((payment = readPayments.readLine()) != null) {
-            list = new ArrayList<>();
-            payment = payment.toLowerCase();
-            while ((debtor = readDebts.readLine()) != null) {
-                debtor = debtor.toLowerCase();
-                paymentsShort = payment.split(";")[1];
-                debtorShort = debtor.split(";")[1];
-                if (debtorShort.contains(paymentsShort)) list.add(debtor);
-        }
-            if (list.size() != 0) {
-             txtResults.setText("Client: \n" + payment + "\nDebtors: ");
-             result.write("Client: " + payment + "\nDebtors: \n");
-                for (int i = 0; i < list.size(); i++) {
-                    txtResults.setText(list.get(i) + "\n\n");
-                    result.write(list.get(i) + "\n\n");
-                }
-             }
-        }
-        readDebts.close();
-        readPayments.close();
-        result.close();}
-        catch (IOException e){
-
-        }
+       debtorsFile = fc.showOpenDialog(null);
+      if(debtorsFile!=null){
+          txtPathDebtors.setText(debtorsFile.getPath());
+      }
     }
 }
