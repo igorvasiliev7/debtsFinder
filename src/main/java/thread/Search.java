79dda5d29a8828a00e2e5fa7d214f8c59029028e
igor <igorvasiliev7@gmail.com>
2018-11-12 17:55:21 +0200

    13

--- src/main/java/thread/Search.java
+++ src/main/java/thread/Search.java
@@ -6,10 +6,12 @@ import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -40,9 +42,7 @@ public class Search implements Runnable {
         try(BufferedWriter bw = Files.newBufferedWriter(Paths.get("result.csv"), StandardCharsets.UTF_8, StandardOpenOption.CREATE);
             BufferedReader brPayment = new BufferedReader(new InputStreamReader(
                     new FileInputStream(paymentsFile), "UTF-8"));
-            BufferedReader brDebts = new BufferedReader(new InputStreamReader(
-                    new FileInputStream(debtorsFile), "UTF-8"));) {
-
+           ) {
             final CSVParser parser = new CSVParserBuilder()
                     .withSeparator(';')
                     .build();
@@ -52,14 +52,25 @@ public class Search implements Runnable {
             CSVWriter csvWriter = new CSVWriter(bw);
             String[] payment;
             String[] debtor;
+            int i=0;
+            int j = 0;
+
             while ((payment = readPayments.readNext()) != null) {
+                ++i;
+                System.out.println(Arrays.toString(payment)+"p"+i);
+
+                BufferedReader brDebts = new BufferedReader(new InputStreamReader(
+                        new FileInputStream(debtorsFile), "UTF-8"));
                 CSVReader readDebts = new CSVReaderBuilder(brDebts)
                         .withCSVParser(parser)
                         .build();
+
                 list.clear();
                 list.add(payment);
 
                 while ((debtor = readDebts.readNext()) != null) {
+//                    ++j;
+//                    System.out.println(Arrays.toString(debtor)+"d"+j);
                     if (debtor[1].toLowerCase().contains(payment[2].toLowerCase())) {
                         list.add(debtor);
                     }
@@ -71,12 +82,16 @@ public class Search implements Runnable {
                       csvWriter.writeNext(strings);
                     }
                 }
+
+                brDebts.close();
+                readDebts.close();
             }
             System.out.println("The End");
             txtResults.appendText("The end");
+            txtError.setText("The end");
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Error in result.txt creating");
+            System.out.println("Error in result.csv creating");
         }
     }
 }
